51b269c4a4b39e5d79e190f327257f12fa228bff
sasaplus1 <sasaplus1@gmail.com>
2021-04-26 01:01:02 +0900

    add download-gh.sh

---
+++ download-gh.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -euo pipefail
+
+__main() {
+  unset -f __main
+
+  local dir=
+
+  if [ -z "$dir" ]
+  then
+    dir="$PWD"
+  else
+    dir="$1"
+  fi
+
+  if [ ! -d "$dir" ]
+  then
+    echo "$dir is not a directory" >&2
+    return 1
+  fi
+
+  local arch=
+
+  case "$(uname -m)" in
+    *386)   arch=386   ;;
+    arm64)  arch=arm64 ;;
+    armv*)  arch=armv6 ;;
+    x86_64) arch=amd64 ;;
+  esac
+
+  local kernel=
+
+  case "$(uname -s)" in
+    Darwin) kernel=macOS ;;
+    Linux)  kernel=linux ;;
+  esac
+
+  local version=
+  version=1.9.2
+
+  local url=
+  url="https://github.com/cli/cli/releases/download/v${version}/gh_${version}_${kernel}_${arch}.tar.gz"
+
+  local tgz=
+  tgz="${dir}/gh.tar.gz"
+
+  curl -fsSL -o "$tgz" "$url"
+  mkdir -p "${dir}/gh"
+  tar fx "$tgz" -C "${dir}/gh" --strip-components 1
+}
+__main "$@"
+
+# vim:list:ts=2
